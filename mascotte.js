a87f1c3ca2d32345780a4f9aad73afbb3e2d5630
dingodorone <dingodorone@gmail.com>
2026-09-23 22:24:11 +0200

    Active Webpushr sur les pages principales

--- mascotte.js
+++ mascotte.js
@@ -50,3 +50,10 @@
   });
   setTimeout(hide, 9500);
 })();
+
+
+if (!document.querySelector('script[src="webpushr.js"]')) {
+  const webpushrScript = document.createElement('script');
+  webpushrScript.src = 'webpushr.js';
+  document.head.appendChild(webpushrScript);
+}
